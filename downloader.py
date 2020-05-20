0af719ec1fe12e0f05a5b0e2ef6d8dd43ecc4ef2
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-20 09:50:33 -0400

    initial commit

---
+++ downloader.py
@@ -0,0 +1,45 @@
+import requests as re
+import json
+import sys
+
+
+##################### SCHEDGE #####################
+
+ROOT_URL = "https://schedge.a1liu.com/"
+SUBJECT_URL = ROOT_URL + "subjects"
+sems = ["fa", "su", "sp", "ja"]
+res = re.get(SUBJECT_URL)
+contents = json.loads(res.content)
+data = []
+
+sem = sys.argv[1]
+year = sys.argv[2]
+full = False
+if len(sys.argv) == 4:
+    full = True
+if sem not in sems:
+    raise ValueError("Invalid semester for " + sem)
+
+#handle error later
+for subject in contents:
+    for subjectCode in contents[subject]:
+        print("Retrieving course data for " + subjectCode + "-" + subject)
+        result = re.get(ROOT_URL + year + "/" + sem + "/" + subject + "/" + subjectCode)
+        if full:
+            result = re.get(ROOT_URL + year + "/" + sem + "/" + subject + "/" + subjectCode + "?full=true")
+        courses = json.loads(result.content)
+        if not courses:
+            print("No available data for " + subjectCode + "-" + subject)
+        else:
+            data.append(courses)
+
+file = None
+if full:
+    file = open("data/" + year + sem + "full.json", "w")
+else:
+    file = open("data/" + year + sem + ".json", "w")
+with file as f:
+    for course in data:
+        f.write("%s\n" % course)
+
+if file: file.close()
